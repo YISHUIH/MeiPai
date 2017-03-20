431868c21a69f41566e5281f85d318c19e11ea6a
YISHUIH <1070379530@qq.com>
2017-03-20 09:21:34 +0800

    toolbar设置

--- app/src/main/java/com/example/administrator/meipai/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/meipai/activity/MainActivity.java
@@ -1,15 +1,65 @@
 package com.example.administrator.meipai.activity;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.administrator.meipai.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tb_videoType)
+    Toolbar tbVideoType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initToolBar();
+    }
+
+    //初始化标题栏
+    private void initToolBar() {
+        setSupportActionBar(tbVideoType);
+
+       ActionBar action = getSupportActionBar();
+        if (action!=null){
+            action.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.pet:
+
+                break;
+
+            case R.id.star:
+
+                break;
+
+            case R.id.travel:
+
+                break;
+        }
+
+        return true;
     }
 }
